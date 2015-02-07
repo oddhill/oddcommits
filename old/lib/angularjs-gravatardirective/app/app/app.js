4bb0a471fdc54db8f9ec36cac6988b41b7810207
Olof Johansson <olof.johansson@oddhill.se>
2015-02-07 17:25:14 +0100

    Erased it all.

--- old/lib/angularjs-gravatardirective/app/app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['md5', 'ui-gravatar']);
-
-function myController($scope) {
-    $scope.email = 'jlavin@jimlavin.net';
-}
