5974629fcca2c257375537bdfa87ed388c8b0f36
Olof Johansson <olof.johansson@oddhill.se>
2015-02-11 20:03:55 +0100

    Removed things.

--- server/routes.js
+++ server/routes.js
@@ -10,8 +10,7 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/changesets', require('./api/changeset'));
-  app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
