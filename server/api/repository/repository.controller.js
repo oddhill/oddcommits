3fa87a16e2a0c321be775272346e8392326724f1
Olof Johansson <olof.johansson@oddhill.se>
2015-02-21 23:26:59 +0100

    Fixed typo.

--- server/api/repository/repository.controller.js
+++ server/api/repository/repository.controller.js
@@ -2,7 +2,7 @@
 
 var beanstalk = require('../../components/beanstalk');
 
-// Get list of repositorys
+// Get list of repositories
 exports.index = function(req, res) {
   beanstalk.get('repositories.json', function(error, response, body) {
     res.json(body);
