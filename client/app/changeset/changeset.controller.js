6430e9fd2cbe554417b9acda447e6599046a68cd
Olof Johansson <olof.johansson@oddhill.se>
2015-02-21 22:52:23 +0100

    Update the Changeset scope with new commits.

--- client/app/changeset/changeset.controller.js
+++ client/app/changeset/changeset.controller.js
@@ -3,4 +3,9 @@
 angular.module('oddcommitsApp')
   .controller('ChangesetCtrl', function ($scope) {
     $scope.commits = {};
+
+    // Watch for the new-commit event, and add each commit to the scope.
+    $scope.$on('new-commit', function(event, commit) {
+      $scope.commits[commit.revision] = commit;
+    });
   });
